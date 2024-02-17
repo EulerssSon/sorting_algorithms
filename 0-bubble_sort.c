40796ed7d43dba84eca95564dc18435f555a31d4
EulerssSon <alfy.kerolous@gmail.com>
2024-02-17 04:16:25 -0800

    feature

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,6 +27,7 @@ void bubble_sort(int *arr, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
+	char is_sorted = 'T';
 
 	if (arr == NULL || size == 0)
 	{
@@ -35,13 +36,19 @@ void bubble_sort(int *arr, size_t size)
 
 	for (i = 0; i < size - 1; ++i)
 	{
+		is_sorted = 'T';
 		for (j = 0; j < size - i - 1; ++j)
 		{
 			if (arr[j + 1] < arr[j])
 			{
 				swap(&arr[j + 1], &arr[j]);
 				print_array(arr, size);
+				is_sorted = 'F';
 			}
+		}
+		if (is_sorted == 'T')
+		{
+			break;
 		}
 	}
 }
